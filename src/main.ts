62454a5a30e68b575b389a3060860e1569cc8c23
Vitor Gonçalves Piovezam <vitorpiovezam@yandex.com>
2019-06-26 20:38:25 -0300

    set main func async and get users

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,42 @@ import * as express from 'express';
 import { credentials } from './core/config/enviroment';
 import { MongoClient } from 'mongodb';
 import * as assert from 'assert';
+import { User } from './core/models/user.model';
 
-const app = express();
-const port = process.env.PORT || 8080;
+async function run() {
+    const app = express();
+    const port = process.env.PORT || 8000;
 
-const apiUrl = `mongodb://${credentials.username}:${credentials.password}@ds243607.mlab.com:43607/cubo-api`;
-const dbName = 'cubo-api';
+    const apiUrl = `mongodb://${credentials.username}:${credentials.password}@ds243607.mlab.com:43607/cubo-api`;
+    const dbName = 'cubo-api';
 
-MongoClient.connect(apiUrl, { useNewUrlParser: true }, (err, client) => {
-    console.log('Connected successfully to server at ' + new Date());
+    const client = await MongoClient.connect(apiUrl, { useNewUrlParser: true });
     const db = client.db(dbName);
-    client.close();
-});
 
-app.get('/', (req, res) => {
-    res.send('ta funfando /o/');
-});
+    app.get('/', (req, res) => {
+        res.send('ta funfando /o/');
+    });
 
-app.get('/users', (req, res) => {
-    
-});
+    app.get('/users', async (req, res) => {
+        let response: User[] = [];
 
-app.listen(port, () => {
-    console.log('Listening on port ' + port);
-});
+        const cursor = await db.collection('users').find();
+        
+        await cursor.forEach(user => {
+            response.push(Object.assign(new User(), user));
+        });
+
+        res.json(response);
+    });
+
+    app.post('/users', (req, res) => {
+       
+    });
+
+    app.listen(port, () => {
+        console.log('Listening on port ' + port);
+    });
+
+};
+
+run();
